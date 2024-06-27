5f7eb91f02f2a2e62045371e877e9f59353d0d68
arian-at <ar.atamantschuk@gmail.com>
2024-06-27 12:52:24 +0200

    Add files via upload

--- Tag.java
+++ Tag.java
@@ -4,12 +4,12 @@ import java.time.LocalDate;
 
 public class Tag {
 	
-	LocalDate datum;
-	int schritte;
-	double[] kalPlus;
-	double[] kalMinus;
-	double kalStand;
-	//Aktivitaet[] akt; -> Klasse noch nicht implementiert
+	LocalDate datum = LocalDate.now();
+	int schritte = 0;
+	double[] kalPlus = new double[0];
+	double[] kalMinus = new double[0];
+	double kalStand = 0;
+	//Aktivitaet[] akt = new Akt[]; -> Klasse noch nicht implementiert
 	
 	/*
 	 * Konstruktore
@@ -20,7 +20,7 @@ public class Tag {
 		
 	}
 	
-	//Konstruktor mit allen Attributen bis auf akt (Klasse fehtl noch)
+	//Konstruktor mit allen Attributen bis auf akt (Klasse fehlt noch)
 	public Tag (LocalDate datum, int schritte, double[] kalPlus, double[] kalMinus,
 	double kalStand) {
 		this.datum = datum;
@@ -39,9 +39,9 @@ public class Tag {
 	 * isTag: überprüft, ob ein Tag-Objekt für den aktuellen Tag vorhanden ist
 	 * im Tag[]-Array des Users 
 	 */
-	public boolean isTag(LocalDate datum, Tag[] tage){
+	public boolean isTag(Tag[] tage){
 		for(int i=0; i<tage.length; i++) {
-			if ( tage[i].datum == datum) {
+			if ( tage[i].datum.equals(LocalDate.now())) {
 				return true;
 			}
 		}
@@ -65,7 +65,6 @@ public class Tag {
 		return schritte;
 	}
 
-	//Set-Schritte (Task am Kanban-Board)
 	public void setSchritte(int schritte) {
 		this.schritte = schritte;
 	}
@@ -74,23 +73,65 @@ public class Tag {
 		return kalPlus;
 	}
 
-	public void setKalPlus(double[] kalPlus) {
-		this.kalPlus = kalPlus;
+	/*
+	 * setKalPlus
+	 * Ersatzarray wird erstellt, der um 1 länger ist als this
+	 * Array wird ins Ersatzarray kopiert, neuer Eintrag am Ende eingetragen
+	 */
+	public void setKalPlus(double kalPlus) {
+		double[] ersatz = new double[this.kalPlus.length+1];
+		for(int i = 0; i < ersatz.length; i++) {
+			if(i==ersatz.length-1) {
+				ersatz[i]=kalPlus;
+			} else {
+				ersatz[i]=this.kalPlus[i];
+			}
+		}
+		this.kalPlus=ersatz;
 	}
 
 	public double[] getKalMinus() {
 		return kalMinus;
 	}
 
-	public void setKalMinus(double[] kalMinus) {
-		this.kalMinus = kalMinus;
+	/*
+	 * setKalMinus
+	 * Ersatzarray wird erstellt, der um 1 länger ist als this
+	 * Array wird ins Ersatzarray kopiert, neuer Eintrag am Ende eingetragen
+	 */
+	public void setKalMinus(double kalMinus) {
+		double[] ersatz = new double[this.kalMinus.length+1];
+		for(int i = 0; i < ersatz.length; i++) {
+			if(i==ersatz.length-1) {
+				ersatz[i]=kalMinus;
+			} else {
+				ersatz[i]=this.kalMinus[i];
+			}
+		}
+		this.kalMinus=ersatz;
 	}
 
 	public double getKalStand() {
 		return kalStand;
 	}
 
-	public void setKalStand(double kalStand) {
-		this.kalStand = kalStand;
+	/*
+	 * setKalStand
+	 * Summe der Kalorieneinahme und Summe des Kalorienverbrauchs des Tages wird
+	 * berrechnet und setzt den Stand als Attribut des Tages
+	 */
+	public void setKalStand(double[] kalPlus, double[] kalMinus) {
+		double kalPlusSum = 0;
+		double kalMinusSum = 0;
+		
+		for(int i = 0; i < kalPlus.length; i++) {
+			kalPlusSum=kalPlusSum+kalPlus[i];
+		}
+		
+		for(int i = 0; i < kalMinus.length; i++) {
+			kalMinusSum= kalMinusSum+kalMinus[i];
+		}
+		
+		this.kalStand = kalPlusSum-kalMinusSum;
 	}
 }
