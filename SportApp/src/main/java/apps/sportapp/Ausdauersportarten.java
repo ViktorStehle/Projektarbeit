0b6eb1177b5aeb2716760f210a4f95d52d9fd2bc
LiesegangF <117732313+LiesegangF@users.noreply.github.com>
2024-07-28 15:36:05 +0200

    Update Ausdauersportarten.java

--- SportApp/src/main/java/apps/sportapp/Ausdauersportarten.java
+++ SportApp/src/main/java/apps/sportapp/Ausdauersportarten.java
@@ -3,20 +3,23 @@ package apps.sportapp;
 public class Ausdauersportarten extends Sportaktivität {
     private float distance;
 
+// Konstruktor zur Initialisierung der Ausdauersportarten-Objekte
     public Ausdauersportarten(User user, int time, float dist) {
         super(user, time);
         this.distance = dist;
     }
 
+// Methode zur Rückgabe der Distanz
     public float getDistance() {
         return distance;
     }
 
-
+// Methode zur Berechnung der Geschwindigkeit (wird in Unterklasse überschrieben)
     public float getPace() {
         return 0; // Default implementation
     }
-    
+
+// Methode zum Setzen der Distanz
     public void setDistanz(float dist) {
         this.distance = dist;
     }
