daf6665e1901fb09c7073696703a185b19833bc2
LiesegangF <117732313+LiesegangF@users.noreply.github.com>
2024-07-28 16:04:53 +0200

    Update Joggen.java

--- SportApp/src/main/java/apps/sportapp/Joggen.java
+++ SportApp/src/main/java/apps/sportapp/Joggen.java
@@ -4,6 +4,7 @@ public class Joggen extends Ausdauersportarten {
     private float kcal;
     private float pace; // km / h 
 
+    //Konstruktor für Klasse Joggen
     public Joggen(User user, int time, float dist) {
         super(user, time, dist);
         this.pace = dist / time * 60;
@@ -11,15 +12,18 @@ public class Joggen extends Ausdauersportarten {
     }
 
     @Override
+    // Getter-Methode für Kilokalorien
     public float getKcal() {
         return kcal;
     }
  
     @Override
+    // Getter-Methode für Geschwindigkeit
     public float getPace() {
         return pace;
     }
     
+    // Setter-Methode für Kilokalorien
     public void setKcal(User user, float time, float dist) {
     	this.kcal = (float) (0.75 * (dist / time * 60) * user.getWeight() * (time / 60));
     }
