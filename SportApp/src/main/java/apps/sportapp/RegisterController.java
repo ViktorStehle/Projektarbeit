2e6e6946d892729abf3cfa92fddf68fd47935017
LiesegangF <117732313+LiesegangF@users.noreply.github.com>
2024-07-28 16:23:33 +0200

    Update RegisterController.java

--- SportApp/src/main/java/apps/sportapp/RegisterController.java
+++ SportApp/src/main/java/apps/sportapp/RegisterController.java
@@ -16,25 +16,26 @@ import java.util.List;
 public class RegisterController {
 
     @FXML
-    private ChoiceBox<String> genderChoiceBox;
+    private ChoiceBox<String> genderChoiceBox; // Auswahlbox für das Geschlecht
 
     @FXML
-    private TextField weightField;
+    private TextField weightField; // Textfeld für das Gewicht
 
     @FXML
-    private DatePicker birthDatePicker;
+    private DatePicker birthDatePicker; // DatePicker für das Geburtsdatum
 
     @FXML
-    private TextField heightField;
+    private TextField heightField; // Textfeld für die Körpergröße
 
     @FXML
-    private TextField usernameField;
+    private TextField usernameField; // Textfeld für den Benutzernamen
 
     @FXML
-    private TextField passwordField;
+    private TextField passwordField; // Textfeld für das Passwort
 
-    protected static List<User> users = new ArrayList<>();
+    protected static List<User> users = new ArrayList<>(); // Liste zur Speicherung der Benutzer
 
+    // Methode zur Verarbeitung der Registrierung
     @FXML
     private void handleSubmit() {
         String gender = genderChoiceBox.getValue();
@@ -44,22 +45,25 @@ public class RegisterController {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
+        // Überprüfen, ob die Eingaben gültig sind
         if (!validateInputs(username, password, gender, weight, height, birthDate)) {
             return;
         }
 
-        User newUser = null;  // Deklarieren Sie newUser außerhalb des try-Blocks
+        User newUser = null;  // Deklarieren des neuen Benutzers außerhalb des try-Blocks
 
         try {
+            // Überprüfen, ob der Benutzername bereits vergeben ist
             boolean userExists = users.stream().anyMatch(user -> user.getName().equals(username));
             if (userExists) {
                 showAlert(Alert.AlertType.ERROR, "Fehler", "Benutzername ist bereits vergeben.");
                 return;
             }
 
+            // Neuen Benutzer erstellen und zur Liste hinzufügen
             newUser = new User(username, password, gender, birthDate, Integer.parseInt(weight), Integer.parseInt(height));
             users.add(newUser);
-            UserFileUtil.saveUsers(users);
+            UserFileUtil.saveUsers(users); // Benutzerliste speichern
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,6 +74,7 @@ public class RegisterController {
         showSuccessAlert(newUser);  // Übergeben Sie newUser an showSuccessAlert
     }
 
+    //Erfolg-Meldung bei erfolgreicher Registrierung
     private void showSuccessAlert(User newUser) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setHeaderText("Erfolgreich");
@@ -92,6 +97,7 @@ public class RegisterController {
         }
     }
 
+    // Methode zur Validierung der Eingaben
     private boolean validateInputs(String username, String password, String gender, String weight, String height, LocalDate birthDate) {
         if (username.isEmpty() || password.isEmpty() || gender == null || gender.isEmpty()) {
             showAlert(Alert.AlertType.ERROR, "Fehler", "Bitte füllen Sie alle Felder korrekt aus.");
@@ -116,6 +122,7 @@ public class RegisterController {
         return true;
     }
 
+    // Methode zur Anzeige von Fehlermeldungen
     private void showAlert(Alert.AlertType alertType, String title, String content) {
         Alert alert = new Alert(alertType);
         alert.setHeaderText(title);
@@ -123,6 +130,7 @@ public class RegisterController {
         alert.showAndWait();
     }
 
+    // Methode zum Öffnen des Hauptfensters der Anwendung
     private void openMainWindow() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SportAppMainScene.fxml"));
@@ -135,11 +143,13 @@ public class RegisterController {
         }
     }
 
+    // Methode zum Schließen des Registrierungsfensters
     private void closeRegisterWindow() {
         Stage stage = (Stage) usernameField.getScene().getWindow();
         stage.close();
     }
 
+    // Methode zum Setzen der Benutzerliste
     public static void setUsers(List<User> usersList) {
         users = usersList;
     }
