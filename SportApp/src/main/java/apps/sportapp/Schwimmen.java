7ae4d6458e12708639af9b28e26e51b03e35704b
LiesegangF <117732313+LiesegangF@users.noreply.github.com>
2024-07-28 16:25:05 +0200

    Update Schwimmen.java

--- SportApp/src/main/java/apps/sportapp/Schwimmen.java
+++ SportApp/src/main/java/apps/sportapp/Schwimmen.java
@@ -1,26 +1,31 @@
 package apps.sportapp;
 
+// Die Klasse Schwimmen erweitert die Klasse Ausdauersportarten
 public class Schwimmen extends Ausdauersportarten {
     private float kcal;
     private float pace; // m / min
 
+    // Konstruktor zur Initialisierung der Schwimmen-Objekte
     public Schwimmen(User user, int time, float dist) {
         super(user, time, dist);
         this.pace = dist / time * 60;
         setKcal(user, time, dist);
     }
 
+    // Überschreibt die getKcal-Methode aus der Oberklasse
     @Override
     public float getKcal() {
         return kcal;
     }
 
+    // Überschreibt die getPace-Methode aus der Oberklasse
     @Override
     public float getPace() {
         return pace;
     }
     
 
+    // Methode zur Berechnung und Setzung der verbrannten Kalorien
     public void setKcal(User user, float time, float dist) {
     	this.kcal = (2 * (dist / time) * user.getWeight() * time);
     }
