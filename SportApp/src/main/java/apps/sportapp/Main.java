4b5b23dc8309967864682321d86ace8ba0d5916e
LiesegangF <117732313+LiesegangF@users.noreply.github.com>
2024-07-28 16:16:23 +0200

    Update Main.java

--- SportApp/src/main/java/apps/sportapp/Main.java
+++ SportApp/src/main/java/apps/sportapp/Main.java
@@ -9,15 +9,20 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
 
+// Die Main-Klasse ist der Einstiegspunkt der SportApp-Anwendung
 public class Main extends Application {
 
+    // Konstante zur Speicherung des Pfades zur Benutzerdatei
     public static final String USER_FILE_PATH = Paths.get("").toAbsolutePath().toString() + "/users.dat";
 
+    // Die Startmethode wird aufgerufen, wenn die Anwendung gestartet wird
     @Override
     public void start(Stage stage) throws IOException, ClassNotFoundException {
+        // Liest die Benutzerinformationen aus der Datei und setzt sie im RegisterController
         List<User> users = UserFileUtil.readUsers();
         RegisterController.setUsers(users);
 
+        // Lädt die Login-Szene und zeigt sie an
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("SportApp - Anmeldung");
@@ -25,6 +30,7 @@ public class Main extends Application {
         stage.show();
     }
 
+    // Die Stoppmethode wird aufgerufen, wenn die Anwendung beendet wird
     @Override
     public void stop() throws IOException {
         UserFileUtil.saveUsers(RegisterController.users);
